0c7b06886a95d7bb4f375b3c9e553fd1a63e0680
Pradeep Pillai <pradeep.pillai@jumio.com>
2020-04-30 11:22:35 +0530

    adding interactivity-step1

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -4,16 +4,33 @@ import Html exposing (div, h1, img, text)
 import Html.Attributes exposing (..)
 
 
+
+--model
+
+
+urlPrefix =
+    "http://elm-in-action.com/"
+
+
+initialModel =
+    [ { url = "1.jpeg" }
+    , { url = "2.jpeg" }
+    , { url = "3.jpeg" }
+    ]
+
+
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
         , div [ id "thumbnails" ]
-            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-            , img [ src "http://elm-in-action.com/2.jpeg" ] []
-            , img [ src "http://elm-in-action.com/3.jpeg" ] []
+            [ List.map viewThumbnail model
             ]
         ]
 
 
 main =
-    view "no model yet"
+    view initialModel
+
+
+viewThumbnail thumbnail =
+    img [ src urlPrefix ++ thumbnail.url ] []
