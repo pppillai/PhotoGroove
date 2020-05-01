f404b4a3aeb4a59a9b9c2f041f5d55af1dc3f39c
Pradeep Pillai <pradeep.pillai@jumio.com>
2020-05-01 10:53:51 +0530

    adding type annotation part 1

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,19 +1,21 @@
 module PhotoGroove exposing (main)
 
-import Html exposing (div, h1, img, text)
+import Browser
+import Html exposing (Html, div, h1, img, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
-import Browser
 
 
 
 --model
 
 
+urlPrefix : String
 urlPrefix =
     "http://elm-in-action.com/"
 
 
+initialModel : { photos : List { url : String }, selectedUrl : String }
 initialModel =
     { photos =
         [ { url = "1.jpeg" }
@@ -27,19 +29,21 @@ initialModel =
 
 -- view
 
+
+view : model -> Html msg
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
         , div [ id "thumbnails" ]
-             -- List.map viewThumbnail model
-             -- (List.map (\photo -> viewThumbnail model.selectedUrl photo) model.photos)
-             (List.map (viewThumbnail model.selectedUrl) model.photos)
-            , img [ class "large", src (urlPrefix ++ "large/" ++ model.selectedUrl) ]
-                []
-
+            -- List.map viewThumbnail model
+            -- (List.map (\photo -> viewThumbnail model.selectedUrl photo) model.photos)
+            (List.map (viewThumbnail model.selectedUrl) model.photos)
+        , img [ class "large", src (urlPrefix ++ "large/" ++ model.selectedUrl) ]
+            []
         ]
 
 
+viewThumbnail : String -> { url : String } -> Html msg
 viewThumbnail selectedUrl thumbnail =
     --if selectedUrl == thumbnail.url then
     --    img [ src (urlPrefix ++ thumbnail.url)
@@ -49,23 +53,32 @@ viewThumbnail selectedUrl thumbnail =
     img
         [ src (urlPrefix ++ thumbnail.url)
         , classList [ ( "selected", selectedUrl == thumbnail.url ) ]
-        , onClick { description = "ClickedPhoto", data = thumbnail.url } ]
+        , onClick { description = "ClickedPhoto", data = thumbnail.url }
+        ]
         []
+
 
 
 -- update
 
+
+update : msg -> model -> { model | selectedUrl : String }
 update msg model =
     if msg.description == "ClickedPhoto" then
         { model | selectedUrl = msg.data }
+
     else
         model
 
+
+
 -- main
+
+
+main : Program () Html msg
 main =
     Browser.sandbox
-    { init = initialModel
-    , view = view
-    , update = update
-    }
-
+        { init = initialModel
+        , view = view
+        , update = update
+        }
